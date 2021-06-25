97ac1e9db8c90427ca6dcf457d95b02c65f878ac
geromegrignon <gerome.grignon.lp2@gmail.com>
2021-06-25 16:44:13 +0200

    feat: add marked support

---
+++ src/app/pipes/marked.pipe.ts
@@ -0,0 +1,20 @@
+import { NgModule, Pipe, PipeTransform } from '@angular/core';
+import * as marked from 'marked';
+
+@Pipe({
+  name: 'marked',
+})
+export class MarkedPipe implements PipeTransform {
+  transform(value: any): any {
+    if (value && value.length > 0) {
+      return marked(value);
+    }
+    return value;
+  }
+}
+
+@NgModule({
+  declarations: [MarkedPipe],
+  exports: [MarkedPipe],
+})
+export class MarkedModule {}
