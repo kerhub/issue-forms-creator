76e445d64091581513469867e52b6572b12c06dd
geromegrignon <gerome.grignon.lp2@gmail.com>
2021-06-25 19:48:38 +0200

    feat: add material default field options

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IssueFormDirective } from './directives/issue-form.directive';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [AppComponent, IssueFormDirective],
   imports: [BrowserModule, BrowserAnimationsModule, ReactiveFormsModule],
-  providers: [],
+  providers: [
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: { appearance: 'outline' },
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
