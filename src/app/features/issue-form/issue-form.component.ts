892c85daea56181b16d1998313f8a6d30cefc338
geromegrignon <gerome.grignon.lp2@gmail.com>
2021-06-26 12:49:09 +0200

    feat: add issue-creator

--- src/app/features/issue-form/issue-form.component.ts
+++ src/app/features/issue-form/issue-form.component.ts
@@ -1,8 +1,47 @@
 import { Component } from '@angular/core';
+import { CdkDragDrop } from '@angular/cdk/drag-drop';
+import { IssueFormGroup } from '../../forms/issue-form-group';
+import { FormArray, FormGroup } from '@angular/forms';
+
+const yaml = require('js-yaml');
 
 @Component({
   selector: 'app-issue-form',
   templateUrl: './issue-form.component.html',
   styleUrls: ['./issue-form.component.scss'],
 })
-export class IssueFormComponent {}
+export class IssueFormComponent {
+  form: IssueFormGroup = new IssueFormGroup();
+
+  get controls(): FormGroup[] {
+    return (this.form.get('body') as FormArray).controls as FormGroup[];
+  }
+
+  drop(event: CdkDragDrop<string[]>): void {
+    this.moveItemInFormArray(
+      this.form.get('body') as FormArray,
+      event.previousIndex,
+      event.currentIndex,
+    );
+  }
+
+  moveItemInFormArray(formArray: FormArray, fromIndex: number, toIndex: number): void {
+    const dir = toIndex > fromIndex ? 1 : -1;
+
+    const item = formArray.at(fromIndex);
+    for (let i = fromIndex; i * dir < toIndex * dir; i = i + dir) {
+      const current = formArray.at(i + dir);
+      formArray.setControl(i, current);
+    }
+    formArray.setControl(toIndex, item);
+  }
+
+  async copy(): Promise<void> {
+    const formattedIssue = yaml.dump({
+      ...this.form.value,
+      labels: [],
+      assignees: [],
+    });
+    await navigator.clipboard.writeText(formattedIssue);
+  }
+}
--- src/app/features/issue-form/issue-form.module.ts
+++ src/app/features/issue-form/issue-form.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { IssueFormRoutingModule } from './issue-form-routing.module';
 import { IssueFormComponent } from './issue-form.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { IssueCreatorModule } from '../issue-creator/issue-creator.component';
 
 @NgModule({
   declarations: [IssueFormComponent],
-  imports: [CommonModule, IssueFormRoutingModule, MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [
+    CommonModule,
+    IssueFormRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    IssueCreatorModule,
+  ],
 })
 export class IssueFormModule {}
