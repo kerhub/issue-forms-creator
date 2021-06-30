a73b253d7514562946a3f53f660d3b58aaeb6e2a
geromegrignon <gerome.grignon.lp2@gmail.com>
2021-06-30 22:51:21 +0200

    refactor: change contributor list to autocomplete
    with repository provided an autocomplete solution is provided to add assignees

--- src/app/features/issue-creator/top-level-creator/top-level-creator.component.ts
+++ src/app/features/issue-creator/top-level-creator/top-level-creator.component.ts
@@ -13,9 +13,10 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { RepositoryService } from '../../../services/repository.service';
 import { MatSelectModule } from '@angular/material/select';
 import { GithubLabel } from '../../../models/github/github-label';
+import { combineLatest, Observable, Subject } from 'rxjs';
+import { map, takeUntil } from 'rxjs/operators';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { GithubContributor } from '../../../models/github/github-contributor';
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-top-level-creator',
@@ -37,25 +38,32 @@ export class TopLevelCreatorComponent implements OnInit, OnDestroy {
 
   labels$ = this.repositoryService.labels$;
   contributors$ = this.repositoryService.contributors$;
+  filteredContributors$: Observable<GithubContributor[] | undefined> = combineLatest([
+    this.contributorsGithubControl.valueChanges,
+    this.contributors$,
+  ]).pipe(
+    map(([input, contributors]) => {
+      const localAssignees = Array.from(this.assignees);
+      const unusedContributors = contributors?.filter(
+        contributor => !localAssignees.includes(contributor.login),
+      );
+      const filteredContributors = unusedContributors?.filter(contributor =>
+        contributor.login.toLowerCase().includes(input),
+      );
+      return filteredContributors;
+    }),
+  );
   repository$ = this.repositoryService.repository$;
   destroy$: Subject<void> = new Subject<void>();
   separatorKeysCodes: number[] = [ENTER, COMMA];
 
-  constructor(private readonly repositoryService: RepositoryService) {
-    repositoryService.repository$.subscribe(console.log);
-  }
+  constructor(private readonly repositoryService: RepositoryService) {}
 
   ngOnInit() {
     this.labelsGithubControl.valueChanges
       .pipe(takeUntil(this.destroy$))
       .subscribe((labels: GithubLabel[]) => {
         this.form.get('labels')?.setValue(labels.map(label => label.name));
-      });
-
-    this.contributorsGithubControl.valueChanges
-      .pipe(takeUntil(this.destroy$))
-      .subscribe((contributors: GithubContributor[]) => {
-        this.form.get('assignees')?.setValue(contributors.map(contributor => contributor.login));
       });
   }
 
@@ -86,6 +94,7 @@ export class TopLevelCreatorComponent implements OnInit, OnDestroy {
 
   addAssigneeFromInput(event: MatChipInputEvent): void {
     if (event.value) {
+      console.log(event.value);
       this.assignees.add(event.value);
       this.form.get('assignees')?.setValue(Array.from(this.assignees));
       event.chipInput!.clear();
@@ -94,6 +103,12 @@ export class TopLevelCreatorComponent implements OnInit, OnDestroy {
 
   removeAssignee(assignee: string): void {
     this.assignees.delete(assignee);
+  }
+
+  addGithubContributor(): void {
+    this.assignees.add(this.contributorsGithubControl.value);
+    this.form.get('assignees')?.setValue(Array.from(this.assignees));
+    this.contributorsGithubControl.reset();
   }
 }
 
@@ -110,6 +125,7 @@ export class TopLevelCreatorComponent implements OnInit, OnDestroy {
     MatButtonModule,
     MatExpansionModule,
     MatSelectModule,
+    MatAutocompleteModule,
   ],
   exports: [TopLevelCreatorComponent],
 })
