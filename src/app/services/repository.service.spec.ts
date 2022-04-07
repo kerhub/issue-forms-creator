6da947214090a0abf0cb5cdf429d8e46947f0d8b
geromegrignon <gerome.grignon.lp2@gmail.com>
2022-04-07 22:36:55 +0200

    fix: format the repository url to extrat

--- src/app/services/repository.service.spec.ts
+++ src/app/services/repository.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { RepositoryService } from './repository.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormControl } from '@angular/forms';
 
 describe('RepositoryService', () => {
   let service: RepositoryService;
@@ -15,5 +16,16 @@ describe('RepositoryService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should extract the repository name', () => {
+    // Given
+    let spy = jest.spyOn(service, 'loadRepositoryInfo');
+
+    // When
+    service.repositoryValidator()(new FormControl('https://github.com/kerhub/issue-forms-creator'));
+
+    // Then
+    expect(spy).toHaveBeenCalledWith('kerhub/issue-forms-creator');
   });
 });
