b8396e5b6e24434019395333e54668d3c4bd37a7
geromegrignon <gerome.grignon.lp2@gmail.com>
2023-08-14 13:12:15 +0200

    chore: update storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,13 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  core: {
-    builder: 'webpack5',
+
+  framework: {
+    name: '@storybook/angular',
+    options: {}
   },
+
+  docs: {
+    autodocs: true
+  }
 };
